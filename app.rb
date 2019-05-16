105cfd32d124c4179a703c42ca84deb54e6655a5
lisamcc <lisamccormack85@gmail.com>
2019-05-16 16:45:50 +0100

    Implementing hit points

--- app.rb
+++ app.rb
@@ -1,28 +1,30 @@
 require 'sinatra/base'
+require 'player'
 
-class Battle < Sinatra::Base 
+class Battle < Sinatra::Base
 enable :sessions
-  
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    session[:first_player] = params[:first_player]
-    session[:second_player] = params[:second_player]
+    $first_player = Player.new(params[:first_player])
+    $second_player = Player.new(params[:second_player])
     redirect '/play'
   end
-  
+
   get '/play' do
-	@first_player = session[:first_player]
-  	@second_player = session[:second_player]
+	  @first_player = $first_player.name
+  	@second_player = $second_player.name
     erb :play
   end
 
   get '/attack' do
-  	@first_player = session[:first_player]
-  	@second_player = session[:second_player]
+  	@first_player = $first_player
+  	@second_player = $second_player
+    @first_player.attack(@second_player)
   	erb :attack
   end
-  
+
 end
---
+++ lib/player.rb
@@ -0,0 +1,18 @@
+class Player
+
+  DEFAULT_HIT_POINTS = 60
+  attr_reader :name, :points, :hit_points
+
+  def initialize(name, hit_points = DEFAULT_HIT_POINTS)
+    @name = name
+    @hit_points = hit_points
+  end
+
+  def attack(name)
+    name.receive_damage
+  end
+
+  def receive_damage
+    @hit_points -= 10
+  end
+end
